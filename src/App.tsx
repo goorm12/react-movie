a8b545823b3621f63d33a43d58b63ba0fa8b02f4
binwoonki <vinwoonki98@gmail.com>
2024-10-08 17:38:54 +0900

    Fetch: 무료 영화 API 사이트를 이용해 영화 데이터 받아오기 작업 하기,
    평점순으로 받아오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+interface Movie {
+  id: number;
+  medium_cover_image: string;
+  title: string;
+  rating: number;
+}
+
 function App() {
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const movieFetch = async (url: string) => {
+      const response = await fetch(url);
+
+      const data = await response.json();
+
+      setMovieData(data.data.movies);
+    };
+
+    movieFetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+  }, []);
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
+    <>
+      {movieData.map((data: Movie) => (
+        <ul key={data.id}>
+          <li>
+            <img src={data.medium_cover_image} alt="" />
+          </li>
+          <li>{data.title}</li>
+          <li>{data.rating}</li>
+        </ul>
+      ))}
+    </>
   );
 }
 
