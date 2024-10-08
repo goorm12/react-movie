e8975c64cb724d6ad1d0c2ef039b33f9d4f40ffe
binwoonki <vinwoonki98@gmail.com>
2024-10-08 17:21:21 +0900

    vite 설정 타입스크립트 Tailwind 적용

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
