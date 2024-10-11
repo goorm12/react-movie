c4d36a49ef2d76e3ca2642ce1806bc3bc31f5b10
binwoonki <vinwoonki98@gmail.com>
2024-10-11 16:56:21 +0900

    margin, screen , fontSize 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        auto: "0 auto",
+      },
+      fontSize: {
+        xs: "0.8rem",
+        xxs: "0.5rem",
+      },
+      screens: {
+        xs: "500px",
+
+        sm: "640px",
+
+        md: "768px",
+
+        lg: "1024px",
+
+        xl: "1280px",
+
+        "2xl": "1536px",
+      },
+    },
   },
   plugins: [],
 };
