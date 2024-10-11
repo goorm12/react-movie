61874be42ca0171c109fb120c36c66a65c97035d
binwoonki <vinwoonki98@gmail.com>
2024-10-11 13:56:20 +0900

    컴포넌트 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-
-interface Movie {
-  id: number;
-  medium_cover_image: string;
-  title: string;
-  rating: number;
-}
+import MovieList from "./components/MovieList";
 
 function App() {
-  const [movieData, setMovieData] = useState([]);
-
-  useEffect(() => {
-    const movieFetch = async (url: string) => {
-      const response = await fetch(url);
-
-      const data = await response.json();
-
-      setMovieData(data.data.movies);
-    };
-
-    movieFetch("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-  }, []);
-
-  return (
-    <>
-      {movieData.map((data: Movie) => (
-        <ul key={data.id}>
-          <li>
-            <img src={data.medium_cover_image} alt="" />
-          </li>
-          <li>{data.title}</li>
-          <li>{data.rating}</li>
-        </ul>
-      ))}
-    </>
-  );
+  return <MovieList />;
 }
 
 export default App;
