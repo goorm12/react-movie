44c56f86c9f1063d5e05fc3c30603acf525eba50
binwoonki <vinwoonki98@gmail.com>
2024-10-11 16:55:46 +0900

    페이지네이션을 하기 위해 코드 분리

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,45 +1,22 @@
-import { useEffect, useState } from "react";
-import { URL } from "../constants/URL";
+import { Movie } from "./MovieList";
 
-interface Movie {
-  id: number;
-  medium_cover_image: string;
-  title: string;
-  rating: number;
-  year: number;
+interface MovieProps {
+  movie: Movie;
 }
 
-const MovieItem = () => {
-  const [movieData, setMovieData] = useState([]);
-
-  useEffect(() => {
-    const movieFetch = async (url: string) => {
-      const response = await fetch(url);
-
-      const data = await response.json();
-
-      setMovieData(data.data.movies);
-    };
-
-    movieFetch(`${URL}&limit=4`);
-  }, []);
-  console.log(movieData);
+const MovieItem = ({ movie }: MovieProps) => {
   return (
-    <>
-      {movieData.map((data: Movie) => (
-        <div key={data.id} className="flex flex-col gap-1 w-full">
-          <img
-            src={data.medium_cover_image}
-            alt={data.title}
-            className="w-full object-cover"
-          />
+    <div key={movie.id} className="flex flex-col gap-1 w-full">
+      <img
+        src={movie.medium_cover_image}
+        alt={movie.title}
+        className="w-full h-auto object-cover"
+      />
 
-          <span className="truncate ...">{data.title}</span>
-          <span>{data.year}년 개봉</span>
-          <span>평점: {data.rating} / 10</span>
-        </div>
-      ))}
-    </>
+      <span className="truncate ...">{movie.title}</span>
+      <span>{movie.year}년 개봉</span>
+      <span>평점: {movie.rating} / 10</span>
+    </div>
   );
 };
 
